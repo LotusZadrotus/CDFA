5b6aa64a3d6b4d84d4f3df83fddda7f8a14417d6
LotusZadrotus <53489119+LotusZadrotus@users.noreply.github.com>
2022-11-19 14:49:55 +0200

    Initial webapi project

---
+++ back/CDFA_back/Models/ProjectContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CDFA_back.Models
+{
+    public class ProjectContext: DbContext
+    {
+        public ProjectContext(DbContextOptions<ProjectContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
